d4f8d204b489d066882322a3efe232cf1b2535e6
bengwinter <ben.g.winter@gmail.com>
2014-09-09 11:26:36 -0400

    working delete phone function

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,22 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
+
+    //function for deleting phones//
+    $scope.deletePhone = function(phone) {
+      // console.log(phone.age);
+      // console.log($scope.phones);
+      var index = 0;
+      for (var i = 0; i < $scope.phones.length - 1; i++) {
+        if ($scope.phones[i].id === phone.id) {
+          index = i;
+          break;
+        }
+      }
+      console.log(index);
+      $scope.phones.splice(index, 1);
+    }
+
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
